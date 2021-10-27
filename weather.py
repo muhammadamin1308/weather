4637b593553505002b5b404c3c25ce2e34864a31
muhammadamin1308 <79371764+muhammadamin1308@users.noreply.github.com>
2021-10-28 00:03:17 +0500

    converted to exe 50% without dist

--- weather.py
+++ weather.py
@@ -22,3 +22,34 @@ def get_weather(place):
 eel.init("web")
 
 eel.start("weather.html", size=(700, 700))    
+
+
+
+
+
+
+# from pyowm import OWM
+
+# owm = OWM('513c4d20ab89b36b3a99a0d5eadaafaa')
+# place = input("Place:")
+
+# mgr = owm.weather_manager()
+# observation = mgr.weather_at_place(place)
+# w = observation.weather
+# t = w.temperature("celsius")
+# t1 = t['temp']
+# print(t1)
+
+
+
+
+
+
+# import eel 
+
+# @eel.expose
+# def pprint():
+#     print("Hello World!")
+
+# eel.init("web")
+# eel.start("new.html")
